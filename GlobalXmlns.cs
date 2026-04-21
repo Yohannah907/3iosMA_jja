c2014998a989b00f3694a5e031c85291a57d89ef
Yohannah907 <balinasjoanna6@gmail.com>
2026-04-21 22:38:07 +0800

    Update GlobalXmlns.cs

--- GlobalXmlns.cs
+++ GlobalXmlns.cs
@@ -1,2 +1,2 @@
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "TeamRing")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "TeamRing.Pages")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "3iosMA_jja")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "3iosMA_jja.Pages")]
