9abd3b6ef8fade4d0d8e3c1668634af3353b4d72
Yohannah907 <balinasjoanna6@gmail.com>
2026-04-21 22:21:59 +0800

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace TeamRing;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OpenScrollViewPage(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ScrollViewPage());
+    }
+
+    private async void OpenListViewPage(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ListViewPage());
+    }
+
+    private async void OpenCollectionViewPage(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new CollectionViewPage());
+    }
+
+    private async void OpenSocialFeedPage(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new SocialFeedPage());
+    }
+}
