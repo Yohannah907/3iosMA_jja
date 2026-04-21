a9641c39244ab61c5622317a0340b0d7613922bf
Yohannah907 <balinasjoanna6@gmail.com>
2026-04-21 22:18:22 +0800

    Add files via upload

---
+++ PostItem.cs
@@ -0,0 +1,13 @@
+﻿namespace TeamRing;
+
+public class PostItem
+{
+    public string Avatar { get; set; } = "";
+    public string Name { get; set; } = "";
+    public string Handle { get; set; } = "";
+    public string Time { get; set; } = "";
+    public string Content { get; set; } = "";
+    public string Likes { get; set; } = "";
+    public string Comments { get; set; } = "";
+    public string Shares { get; set; } = "";
+}
